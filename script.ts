e608a239988c341bde59c5ed261317aad279cf37
aghArdeshir <ardeshirizadinl@gmail.com>
2024-10-29 23:07:47 +0100

    add color palette and background

--- script.ts
+++ script.ts
@@ -1,1 +1,9 @@
-console.log("hi");
+const colorPallete = {
+  text: "#fffcf2",
+  lightgray: "#ccc5b9",
+  darkgray: "#403d39",
+  bg: "#252422",
+  orange: "#eb5e28",
+};
+
+document.body.style.backgroundColor = colorPallete.bg;
