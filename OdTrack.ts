a9c37bb48344a112522d050cde088c3d0807279a
aghArdeshir <ardeshirizadinl@gmail.com>
2024-10-29 23:48:07 +0100

    Add OdTrack web component

---
+++ OdTrack.ts
@@ -0,0 +1,40 @@
+import { Track } from "./Track";
+
+class OdTrack extends HTMLElement {
+  static observedAttributes = ["data-track-index"];
+  private track: Track;
+
+  attributeChangedCallback() {
+    const trackIndexAsString = this.getAttribute("data-track-index");
+
+    if (!trackIndexAsString) {
+      return;
+    }
+
+    const trackIndex = parseInt(trackIndexAsString);
+    this.track = window.opendaw.tracks[trackIndex];
+  }
+
+  connectedCallback() {
+    const shadow = this.attachShadow({ mode: "open" });
+
+    const wrapper = document.createElement("div");
+    wrapper.style.width = "100vw";
+
+    const playButton = document.createElement("button");
+    playButton.textContent = "Play";
+
+    wrapper.appendChild(playButton);
+    playButton.addEventListener("click", this.play.bind(this));
+
+    shadow.appendChild(wrapper);
+  }
+
+  play() {
+    this.track.play();
+  }
+}
+
+export function registerWebCompoenent_OdTrack() {
+  customElements.define("od-track", OdTrack);
+}
