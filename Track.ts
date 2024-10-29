2de62e97351e683a79a47e2216ebb4ffa44b6a18
aghArdeshir <ardeshirizadinl@gmail.com>
2024-10-29 23:58:37 +0100

    tracks have names

--- Track.ts
+++ Track.ts
@@ -1,16 +1,22 @@
 export class Track {
   private file: File;
   private blob: string;
+  public name: string;
 
   constructor() {}
 
   setFile(file: File) {
     this.file = file;
     this.blob = URL.createObjectURL(file);
+    this.name = file.name;
   }
 
   play() {
     const audio = new Audio(this.blob);
     audio.play();
+  }
+
+  setName(name: string) {
+    this.name = name;
   }
 }
